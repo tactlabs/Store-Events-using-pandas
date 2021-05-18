fd3d0adcf741027f8a5400dd9b1389f21d424158
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-05-18 20:49:39 +0530

    code updated

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import time
 import csv 
 from datetime import datetime
 import pandas as pd
@@ -9,16 +8,9 @@ PORT=5000
 @app.route('/', methods=['GET', 'POST'])
 def root():
 	return render_template('index.html')
-    
 
-
-@app.route('/marks', methods=['GET', 'POST'])
-def index():
-	return render_template('index.html')
-
-
-@app.route('/getdetails', methods=['GET', 'POST'])
-def getdetails():
+@app.route('/get_data', methods=['GET', 'POST'])
+def get_data():
     global name
     if request.method == 'POST':
         name = request.form['name']
@@ -35,33 +27,18 @@ def getdetails():
         name3 = request.form['name3']
         date3 = request.form['date3']
         date3=str(date3)
-        
+ 
     data={'Event_Name':[name,name2,name3],'Date':[date,date2,date3]}
     df = pd.DataFrame(data, columns = ['Event_Name', 'Date'])
                              
     print(df)
-    df.to_csv('event.csv', index=False)
+    df.to_csv('data.csv', index=False)
 
-    # f=request.form['event.csv']
-    
-    # # with open(f) as file:
-    # df=csv.reader(df)
-    # for row in df:
-    #  data.append(row)
-    # return render_template('data.html',data=data)        
-
-    # df = pd.read_csv('event.csv')
-    # df.append({'Event Name': name, 'Date': date },ignore_index=True)
-   
+    a=pd.read_csv("data.csv")
+    a.to_html("output.html")
+    return render_template('output.html')
         
-    # return render_template('new.html',df=df.to_csv('event.csv',header=True,index=False))           
-    return render_template('new.html', n=name, s=date,n2=name2,s2=date2,n3=name3,s3=date3) 
-        
-     
-
-    return render_template('index.html')
-    return name	
- 
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=PORT)
+    
